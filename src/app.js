9236b6b5a6dff8b1e34d871807e982bec9e133b7
Jon Olson <jonolson64@outlook.com>
2018-07-28 15:09:04 -0400

    setup production webpack build

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SiderealApp from './components/SiderealApp';
 import 'normalize.css/normalize.css';
-import './styles/styles.scss'
+import './styles/styles.scss';
 
 ReactDOM.render(<SiderealApp />, document.getElementById('root'));
