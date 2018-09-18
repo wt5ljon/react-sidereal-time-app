903bed3bbab9b0c323578003f8beedb075505e7e
Jon Olson <jonolson64@outlook.com>
2018-09-17 20:08:41 -0400

    add time text to ShowDial component

--- src/components/ShowDials.js
+++ src/components/ShowDials.js
@@ -7,13 +7,19 @@ export default class ShowDial extends React.Component {
     const lstTimeRotation = -((this.props.lstHour * 15.0) + (this.props.lstMinute * 0.25));
     
     return (
-      <div className="dials-container" style={this.props.show ? { } : { display: 'none' } }>
-        <div>
-          <img style={{ transform: `rotate(${lstTimeRotation}deg)`}} className="bottomdial" src="/images/clockface_2.png" width="650px" height="650px" />
-          <img style={{ transform: `rotate(${localTimeRotation}deg)`}} className="topdial" src="/images/clockface_3.png" width="550px" height="550px"  />
-          <img className="pointer" src="/images/pointer.png" width="2px" height="321px" />
+      <div>
+        <span className="textData">
+          <h4>Sidereal Time (Outer Scale): {('0' + this.props.lstHour).slice(-2)}:{('0' + this.props.lstMinute).slice(-2)}</h4>
+          <h4>Local Time (Inner Scale): {('0' + this.props.localHour).slice(-2)}:{('0' + this.props.localMinute).slice(-2)}</h4>
+        </span>
+        <div className="dials-container" style={this.props.show ? { } : { display: 'none' } }>
+          <div>
+            <img style={{ transform: `rotate(${lstTimeRotation}deg)`}} className="bottomdial" src="/images/clockface_2.png" width="650px" height="650px" />
+            <img style={{ transform: `rotate(${localTimeRotation}deg)`}} className="topdial" src="/images/clockface_3.png" width="550px" height="550px"  />
+            <img className="pointer" src="/images/pointer.png" width="2px" height="321px" />
+          </div>
         </div>
       </div>
-    );
+      );
   };
 }
