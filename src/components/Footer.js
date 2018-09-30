341918599be772f2f3a5206be8dc1561026f6a37
Jon Olson <jonolson64@outlook.com>
2018-09-30 14:43:29 -0400

    add Google citation logo to the footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,12 @@ const Footer = (props) => {
             <div className="block__details">
                 <span className="row__data row__data--last">
                     <span className="footer__text">Copyright {props.year}</span>
+                    <img
+                      className="googleLogo"
+                      src="/images/powered_by_google_on_white.png"
+                      width="144px"
+                      height="18px"
+                    />  
                     <span className="footer__text">Version: {props.version}</span>
                 </span>
             </div>
